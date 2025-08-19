b28fd13615ae60911074a8c2b679617c19bd9118
mav <miguelangelcardonatangarife@gmail.com>
2025-08-19 17:07:14 -0500

    prueba

--- Back-end/controllers/notificacionController.js
+++ Back-end/controllers/notificacionController.js
@@ -1,6 +1,6 @@
 const Notificacion = require("../models/notificacion");
 
-// crear una notificacion
+// CREAR UNA notificacion
 exports.crearNotificacion = async (req, res) => {
     try {
         const nuevaNotificacion = new Notificacion(req.body);
