779c4a6707f4441214a4f10429d41709262d8476
lisseth <lisisotomonsalve@gmail.com>
2025-09-01 13:49:26 -0500

    perfil_y_app

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+
+//pantallas de autenticación
+import Login from "./auth/Login";
+import Recuperar from "./auth/Recuperar";       
+import NuevaContra from "./auth/Nueva_contra"; 
+
+//pantallas del sistema
+import Dashboard from "./pages/Dashboard";
+import Clientes from "./pages/Clientes";
+import Productos from "./pages/Productos";
+import Facturas from "./pages/Facturas";
+
+//rutas privadas
+const PrivateRoute = ({ children }) => {
+  const token = localStorage.getItem("token");
+  return token ? children : <Navigate to="/login" />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Rutas públicas (auth) */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/recuperar" element={<Recuperar />} />
+        <Route path="/nueva-contra" element={<NuevaContra />} />
+
+        {/* Rutas privadas */}
+        <Route
+          path="/"//en cualquier parte que diga dashboard va el nombre de la pagina inicial
+          element={
+            <PrivateRoute>
+              <Dashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/clientes"
+          element={
+            <PrivateRoute>
+              <Clientes />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/productos"
+          element={
+            <PrivateRoute>
+              <Productos />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/facturas"
+          element={
+            <PrivateRoute>
+              <Facturas />
+            </PrivateRoute>
+          }
+        />
+
+        {/* Redirección inicial */}
+        <Route path="/" element={<Navigate to="/paginainicio" />} />
+
+        {/* 404 */}
+        <Route path="*" element={<h2>404 - Página no encontrada</h2>} />
+      </Routes>
+    </Router>
   );
 }
 
