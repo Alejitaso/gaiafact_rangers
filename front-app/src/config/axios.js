63da69dbc2ea23631ec48eb2e692d485b28dd891
lisseth <lisisotomonsalve@gmail.com>
2025-11-19 10:56:28 -0500

    variable entorno front

--- front-app/src/config/axios.js
+++ front-app/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const ClientesAxios = axios.create({
-  baseURL: "http://localhost:4000/",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // ✅ Interceptor para agregar token a TODAS las peticiones
