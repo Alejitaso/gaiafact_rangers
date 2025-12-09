b08a0e1e5e48707655108b5d16f3a6a98e7de5dc
Mavx13 <miguelangelcardonatangarife@gmail.com>
2025-12-09 14:30:05 -0500

    error recuperar contra 4

--- front-app/src/components/auth/nueva_contra.js
+++ front-app/src/components/auth/nueva_contra.js
@@ -32,7 +32,7 @@ function NewPassword() {
     setSuccess(null);
 
     try {
-      const res = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/reset/${token}`, {
+      const res = await fetch(`${process.env.BACKEND_URL}/api/auth/reset/${token}`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ nuevaPassword: newPassword })
